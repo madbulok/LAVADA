b1eb4d634683938f5552a0fbcb836c62f45a1473
Yulia Valitova <ikskulfg@gmail.com>
2022-02-10 17:36:57 +0300

    вход через фэйсбук. нужно причесывать!

--- app/src/main/java/com/uzlov/dating/lavada/ui/fragments/registration/RegistrationFragment.kt
+++ app/src/main/java/com/uzlov/dating/lavada/ui/fragments/registration/RegistrationFragment.kt
@@ -1,16 +1,22 @@
 package com.uzlov.dating.lavada.ui.fragments.registration
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.util.Patterns
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.uzlov.dating.lavada.R
 import com.uzlov.dating.lavada.app.appComponent
 import com.uzlov.dating.lavada.auth.FirebaseEmailAuthService
@@ -19,12 +25,19 @@ import com.uzlov.dating.lavada.auth.User
 import com.uzlov.dating.lavada.databinding.FragmentRegistrationBinding
 import com.uzlov.dating.lavada.ui.fragments.BaseFragment
 import com.uzlov.dating.lavada.ui.fragments.profile.AboutMyselfFragment
+import org.json.JSONException
+import org.json.JSONObject
 import java.util.regex.Pattern
 import javax.inject.Inject
 
 
 class RegistrationFragment :
     BaseFragment<FragmentRegistrationBinding>(FragmentRegistrationBinding::inflate) {
+
+    private lateinit var callbackManager: CallbackManager
+
+    lateinit var email: String
+    lateinit var firstName: String
 
     private lateinit var googleSignInClient: GoogleSignInClient
 
@@ -50,6 +63,19 @@ class RegistrationFragment :
             }
         }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        callbackManager = CallbackManager.Factory.create()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        Log.d("letsSee", "zzzzzzzzz: $data")
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requireContext().appComponent.inject(this)
@@ -59,7 +85,7 @@ class RegistrationFragment :
             GoogleSignIn.getClient(requireContext(), firebaseEmailAuthService.getGSO(context!!))
 
         viewBinding.btnLogin.setOnClickListener {
-            val email = viewBinding.textInputEmail.text.toString()
+            val email = viewBinding.tiEtEmail.text.toString()
             val password = viewBinding.textInputPassword.text.toString()
             firebaseEmailAuthService.registered(email, password)
             updateUI()
@@ -67,6 +93,97 @@ class RegistrationFragment :
         viewBinding.btnLoginWithGoogle.setOnClickListener {
             onAct()
         }
+
+        viewBinding.btnLoginWithFacebook.setReadPermissions("email")
+        viewBinding.btnLoginWithFacebook.fragment = this
+        viewBinding.btnLoginWithFacebook.registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+            }
+        })
+        arghhhh()
+    }
+fun arghhhh(){
+    LoginManager.getInstance().logInWithReadPermissions(this, listOf("email", "public_profile"))
+
+    LoginManager.getInstance()
+        .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+
+                val token = loginResult.accessToken
+
+                val request = GraphRequest.newMeRequest(
+                    token,
+                    object : GraphRequest.GraphJSONObjectCallback {
+                        override fun onCompleted(
+                            obj: JSONObject?,
+                            response: GraphResponse?
+                        ) {
+
+                            try {
+                                // Save user email to variable
+                                email = obj!!.getString("email")
+                                firstName = obj.getString("first_name")
+                                Log.d(TAG, "1) Facebook email received $email and name $firstName")
+                                handleFacebookAccessToken(token)
+                            }
+                            catch (e: JSONException) {
+                                Toast.makeText(
+                                    context,
+                                    "Facebook Authentication Failed.",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    })
+
+                val parameters = Bundle()
+                parameters.putString("fields", "email,first_name,last_name")
+                request.parameters = parameters
+                request.executeAsync()
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+                Toast.makeText(context, "Facebook Login Cancelled", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+                Toast.makeText(context, "Facebook Authentication Failed", Toast.LENGTH_LONG)
+                    .show()
+            }
+        })
+}
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        firebaseEmailAuthService.auth.signInWithCredential(credential)
+            .addOnCompleteListener(Activity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    updateUI()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(
+                        context, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
     private fun checkUser() {
@@ -108,7 +225,7 @@ class RegistrationFragment :
     }
 
     private fun addTextChangedListener() {
-        viewBinding.textInputEmail.addTextChangedListener(object : TextWatcher {
+        viewBinding.tiEtEmail.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable) {
                 verifyEditText()
             }
@@ -128,7 +245,7 @@ class RegistrationFragment :
 
     private fun verifyEditText() {
         with(viewBinding) {
-            btnLogin.isEnabled = isValidEmail(viewBinding.textInputEmail.text.toString()) &&
+            btnLogin.isEnabled = isValidEmail(viewBinding.tiEtEmail.text.toString()) &&
                     isValidPassword(viewBinding.textInputPassword.text.toString())
         }
     }
