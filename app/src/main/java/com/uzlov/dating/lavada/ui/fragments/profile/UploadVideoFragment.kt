2040a0c37f22b04e8a0106f1ae3ae1494e56df29
Yulia Valitova <ikskulfg@gmail.com>
2022-02-17 19:15:33 +0300

    VideoCompressor

--- app/src/main/java/com/uzlov/dating/lavada/ui/fragments/profile/UploadVideoFragment.kt
+++ app/src/main/java/com/uzlov/dating/lavada/ui/fragments/profile/UploadVideoFragment.kt
@@ -4,10 +4,17 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.abedelazizshe.lightcompressorlibrary.CompressionListener
+import com.abedelazizshe.lightcompressorlibrary.VideoCompressor
+import com.abedelazizshe.lightcompressorlibrary.VideoQuality
+import com.abedelazizshe.lightcompressorlibrary.config.Configuration
 import com.uzlov.dating.lavada.R
 import com.uzlov.dating.lavada.app.appComponent
 import com.uzlov.dating.lavada.databinding.FragmentUploadVideoBinding
@@ -21,6 +28,7 @@ class UploadVideoFragment :
 
     @Inject
     lateinit var firebaseStorageService: FirebaseStorageService
+    lateinit var list: List<Uri>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -44,8 +52,9 @@ class UploadVideoFragment :
                 val uriPathHelper = URIPathHelper()
                 val videoFullPath =
                     data.data?.let { context?.let { it1 -> uriPathHelper.getPath(it1, it) } }
+                list = listOf(data.data) as List<Uri>
                 if (videoFullPath != null) {
-                    firebaseStorageService.uploadVideo(videoFullPath.toString())
+                    compressVideo()
                 }
             }
         }
@@ -79,6 +88,52 @@ class UploadVideoFragment :
             .commit()
 
     }
+
+    private fun compressVideo(){
+        //нужно обновить UI (вопрос что там обновлять), работает на корутинах, не в основном потомке
+            context?.let {
+                VideoCompressor.start(
+                    context = it, // => This is required
+                    uris = list, // => Source can be provided as content uris
+                    isStreamable = true,
+                    saveAt = Environment.DIRECTORY_MOVIES, // => the directory to save the compressed video(s)
+                    listener = object : CompressionListener {
+                        override fun onProgress(index: Int, percent: Float) {
+                            // Update UI with progress value
+                        }
+
+                        override fun onStart(index: Int) {
+                            // Compression start
+                        }
+
+                        override fun onSuccess(index: Int, size: Long, path: String?) {
+                            // On Compression success
+                            Log.d("SUCCESS", "video compressed")
+                            firebaseStorageService.uploadVideo(path.toString(), context!!)
+                        }
+
+                        override fun onFailure(index: Int, failureMessage: String) {
+                            // On Failure
+                            Log.d("Error", "fail(")
+                        }
+
+                        override fun onCancelled(index: Int) {
+                            // On Cancelled
+                        }
+
+                    },
+                    configureWith = Configuration(
+                        quality = VideoQuality.MEDIUM,
+                        frameRate = 24, /*Int, ignore, or null*/
+                        isMinBitrateCheckEnabled = true,
+                        videoBitrate = 3677198, /*Int, ignore, or null*/
+                        disableAudio = false, /*Boolean, or ignore*/
+                        keepOriginalResolution = true, /*Boolean, or ignore*/
+                    )
+                )
+            }
+    }
+
     companion object {
         const val REQUEST_EXTERNAL_STORAGE = 1
         private val PERMISSIONS_STORAGE = arrayOf(
