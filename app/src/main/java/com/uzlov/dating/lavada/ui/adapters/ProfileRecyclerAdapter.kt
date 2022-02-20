2dec7ae8b835e2fac90857d930a616c24e369ea5
Artem <artemisuzl@gmail.com>
2022-02-20 20:35:09 +0300

    Update profile into main screen

--- app/src/main/java/com/uzlov/dating/lavada/ui/adapters/ProfileRecyclerAdapter.kt
+++ app/src/main/java/com/uzlov/dating/lavada/ui/adapters/ProfileRecyclerAdapter.kt
@@ -1,4 +1,5 @@
 package com.uzlov.dating.lavada.ui.adapters
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,95 +12,135 @@ import com.uzlov.dating.lavada.domain.models.User
 import java.util.*
 
 class ProfileRecyclerAdapter(
-        private var modelList: List<User>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
+    private var modelList: List<User>,
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
     PlayerStateCallback {
-        private var mItemClickListener: OnItemClickListener? = null
+    private var mItemClickListener: OnItemClickListener? = null
+    private var actionListener: OnActionListener? = null
 
-        fun updateList(modelList: List<User>) {
-            this.modelList = modelList
-            notifyDataSetChanged()
-        }
+    interface OnActionListener {
+        fun sendGift()
+        fun sendHeart()
+        fun sendMessage()
+    }
 
-        override fun onCreateViewHolder(
-            viewGroup: ViewGroup,
-            viewType: Int
-        ): VideoPlayerViewHolder {
-            val binding: TiktokTimelineItemRecyclerBinding = DataBindingUtil.inflate(LayoutInflater.from(viewGroup.context)
-                , R.layout.tiktok_timeline_item_recycler, viewGroup, false)
-            return VideoPlayerViewHolder(binding)
-        }
+    fun updateList(modelList: List<User>) {
+        this.modelList = modelList
+        notifyDataSetChanged()
+    }
 
-        override fun onBindViewHolder(
-            holder: RecyclerView.ViewHolder,
-            position: Int
-        ) {
+    override fun onCreateViewHolder(
+        viewGroup: ViewGroup,
+        viewType: Int,
+    ): VideoPlayerViewHolder {
+        val binding: TiktokTimelineItemRecyclerBinding =
+            DataBindingUtil.inflate(LayoutInflater.from(viewGroup.context),
+                R.layout.tiktok_timeline_item_recycler,
+                viewGroup,
+                false)
+        return VideoPlayerViewHolder(binding)
+    }
 
-            //Here you can fill your row view
-            if (holder is VideoPlayerViewHolder) {
-                val model = getItem(position)
-                val genericViewHolder = holder
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int,
+    ) {
 
-                // send data to view holder
-                genericViewHolder.onBind(model)
-            }
-        }
+        //Here you can fill your row view
+        if (holder is VideoPlayerViewHolder) {
+            val model = getItem(position)
+            val genericViewHolder = holder
 
-        override fun getItemCount(): Int {
-            return modelList.size
-        }
-
-        private fun getItem(position: Int): User {
-            return modelList[position]
-        }
-
-        override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
-            val position = holder.adapterPosition
-            PlayerViewAdapter.releaseRecycledPlayers(position)
-            super.onViewRecycled(holder)
-        }
-
-        fun setOnItemClickListener(mItemClickListener: OnItemClickListener?) {
-            this.mItemClickListener = mItemClickListener
-        }
-
-        interface OnItemClickListener {
-            fun onItemClick(
-                position: Int,
-                model: User?
-            )
-        }
-
-        inner class VideoPlayerViewHolder(private val binding: TiktokTimelineItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root) {
-            fun onBind(model: User) {
-
-                binding.root.setOnClickListener(object : DoubleClickListener() {
-                    override fun onDoubleClick(v: View) {
-                        mItemClickListener?.onItemClick(
-                            adapterPosition,
-                            model
-                        )
-                    }
-                })
-
-                binding.apply {
-                    dataModel = model
-                    callback = this@ProfileRecyclerAdapter
-                    index = adapterPosition
-                    executePendingBindings()
-                }
-            }
-        }
-
-        override fun onVideoDurationRetrieved(duration: Long, player: Player) {
-        }
-
-        override fun onVideoBuffering(player: Player) {
-        }
-
-        override fun onStartedPlaying(player: Player) {
-
-        }
-
-        override fun onFinishedPlaying(player: Player) {
+            // send data to view holder
+            genericViewHolder.onBind(model)
         }
     }
+
+    override fun getItemCount(): Int {
+        return modelList.size
+    }
+
+    private fun getItem(position: Int): User {
+        return modelList[position]
+    }
+
+    override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+        val position = holder.adapterPosition
+        PlayerViewAdapter.releaseRecycledPlayers(position)
+        super.onViewRecycled(holder)
+    }
+
+    fun setOnActionClickListener(mActionClickListener: OnActionListener?) {
+        actionListener = mActionClickListener
+    }
+
+    fun setOnItemClickListener(mItemClickListener: OnItemClickListener?) {
+        this.mItemClickListener = mItemClickListener
+    }
+
+    fun removeActionListener() {
+        actionListener = null
+    }
+
+    fun removeOnItemSelectListener() {
+        mItemClickListener = null
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(
+            position: Int,
+            model: User?,
+        )
+    }
+
+    inner class VideoPlayerViewHolder(private val binding: TiktokTimelineItemRecyclerBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(model: User) {
+
+            binding.root.setOnClickListener(object : DoubleClickListener() {
+                override fun onDoubleClick(v: View) {
+                    mItemClickListener?.onItemClick(
+                        adapterPosition,
+                        model
+                    )
+                }
+            })
+
+            binding.ivHeartTo.setOnClickListener {
+                actionListener?.sendHeart()
+            }
+            binding.ivGiftTo.setOnClickListener {
+                actionListener?.sendGift()
+            }
+            binding.ivMessageTo.setOnClickListener {
+                actionListener?.sendMessage()
+            }
+
+            binding.apply {
+                dataModel = model
+                callback = this@ProfileRecyclerAdapter
+                index = adapterPosition
+                executePendingBindings()
+            }
+        }
+    }
+
+    override fun onVideoDurationRetrieved(duration: Long, player: Player) {
+    }
+
+    override fun onVideoBuffering(player: Player) {
+    }
+
+    override fun onStartedPlaying(player: Player) {
+
+    }
+
+    override fun onFinishedPlaying(player: Player) {
+    }
+
+    override fun onViewDetachedFromWindow(holder: RecyclerView.ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+//        removeOnItemSelectListener()
+//        removeActionListener()
+    }
+}
---
+++ app/src/main/java/com/uzlov/dating/lavada/ui/fragments/FragmentMatch.kt
@@ -0,0 +1,34 @@
+package com.uzlov.dating.lavada.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.uzlov.dating.lavada.databinding.FragmentMatchBottomLayoutBinding
+
+class FragmentMatch : BottomSheetDialogFragment() {
+    private var viewBinding: FragmentMatchBottomLayoutBinding?= null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewBinding = FragmentMatchBottomLayoutBinding.inflate(inflater, container, false)
+        return viewBinding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding?.btnSendMessageTo?.setOnClickListener {
+
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewBinding = null
+    }
+}
