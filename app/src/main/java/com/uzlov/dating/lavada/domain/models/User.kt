e56e6104345998fb152278631f2befd0881f84fb
Yulia Valitova <ikskulfg@gmail.com>
2022-03-03 19:26:10 +0300

    fix + add location

--- app/src/main/java/com/uzlov/dating/lavada/domain/models/User.kt
+++ app/src/main/java/com/uzlov/dating/lavada/domain/models/User.kt
@@ -9,20 +9,22 @@ enum class MALE {
     WOMAN,
     ANOTHER
 }
+
 @Parcelize
 data class User(
     var uid: String = UUID.randomUUID().toString(),
-    var email: String?= "",
-    var password: String?= "",
-    var name: String?= "",
-    var male: MALE?= null,
-    var age: Int?= 0,
-    var about: String?= "",
-    var url_avatar: String?= "",
-    var url_video: String?= "",
-    var lat: Double?= 0.0,
+    var email: String? = "",
+    var password: String? = "",
+    var name: String? = "",
+    var male: MALE? = null,
+    var age: Int? = 0,
+    var about: String? = "",
+    var url_avatar: String? = "",
+    var url_video: String? = "",
+    var lat: Double? = 0.0,
     var lon: Double? = 0.0,
     var matches: MutableMap<String, Boolean> = mutableMapOf(), // хранит лайки пользователей <UID друга , взаимно или нет>
     var chats: MutableMap<String, String> = mutableMapOf(),// хранит чаты где он есть <UID собеседника , свой UID>
-    var dist: Double? = 0.0 //хранит расстояние между ним и тем, кому его показывать
-): Parcelable
+    var dist: Double? = 0.0, //хранит расстояние между ним и тем, кому его показывать
+    var location: String? = ""
+) : Parcelable
